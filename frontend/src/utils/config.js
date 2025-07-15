937036f8d29f8d4bfbb57ccbaef6db325f96e7b4
santhosh-gopisetti <santhoshgopisetti258@gmail.com>
2025-07-15 14:42:44 +0530

    base

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3050/api";
+const BASE_URL = "https://tripsphere-9dni.onrender.com/";
 // const BASE_URL = "https://trips-travel-server.vercel.app/api";
 
 export default BASE_URL;
